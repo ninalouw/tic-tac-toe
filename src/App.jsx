16cb5804c95483e277d6e769b4ba6dd997decf6a
Nina Louw <ninalouw@mac.com>
2018-08-27 22:16:00 -0700

    add more styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Game from './components/Game';
 
@@ -8,7 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Tic Tac Toe</h1>
         </header>
         <Game />
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -6,9 +6,6 @@ class Game extends React.Component {
     return (
       <div>
         <h2>Play the game!</h2>
-        <div className="gameStatus">
-          {/* display status of game: X or Y to play or winner declared */}
-        </div>
         <div className="gameBoard">
           <Board />
         </div>
