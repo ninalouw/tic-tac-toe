898b988fc75a71a2ccdcfd2d5cea6c32fb70ac9d
Nina Louw <ninalouw@mac.com>
2018-08-27 18:08:07 -0700

    add initial calc winner logic

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -23,10 +23,29 @@ class Board extends React.Component {
       nextPlayerIsX: !this.state.nextPlayerIsX,
     });
   }
+  calculateWinner(squares){
+    var hasWon = false;
+    //want to check first 3 indices
+    console.log(squares);
+    for(var i = 0; i < squares.length; i++){
+        console.log(i);
+        if ((squares[i] === squares[i + 1]) && (squares[i+1] === squares[i + 2]) ||
+        (squares[i] === squares[i + 3]) && (squares[i + 3] === squares[i + 6])){
+          return hasWon = true;
+        }
+        hasWon;
+    }
+  }
 
   render(){
     let nextPlayer = this.state.nextPlayerIsX ? 'X' : 'O';
-    const status = `Next player: ${nextPlayer}`;
+    const winner = this.calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+        status = `Winner:${winner}`;
+    } else {
+        status = `Next player: ${nextPlayer}`;
+    }
 
     return(
       <div className="board">
